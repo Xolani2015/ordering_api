1935869d109dcaec3846dc9727edef61e8883ce8
xolani-pilot <xr.mkhwanazi.20@gmail.com>
2025-05-13 11:59:36 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const Product = require('./models/product.model.js'); // Assuming you have a product model defined in models/product.model.js
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const productRoute = require('./routes/product.route.js'); // Assuming you have a product route defined in routes/product.routes.js
+
+//routes
+app.use('/api/products', require('./routes/product.route.js')); // Assuming you have a product route defined in routes/product.routes.js
+
+app.get('/', (req, res) => {
+  res.send('Hello World! from Express API nodemon 3');
+}); 
+
+
+app.get('/api/products', productRoute);
+
+
+
+
+  
+
+mongoose.connect('mongodb+srv://backendcluster0:XnB3Pu6jnG6ee12b@cluster0.fkkvbai.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => { 
+    console.log('MongoDB connected'); 
+    app.listen(3001, () => {
+      console.log('Server is running on port 3001');
+    });
+}).catch((err) => { console.log('MongoDB connection error:', err); })
---
+++ models/product.model.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [ true, 'Product name is required' ],
+  },
+  description: {
+    type: String,
+    required: [true, 'Product description is required'],
+  },
+
+  quantity: {
+    type: Number,
+    required: [true, 'Product quantity is required'],
+    validate: {
+      validator: function(value) {
+        return value > -1;
+      },
+      message: 'Product quantity cannot be negative',
+    },
+  },
+  
+  price: {
+    type: Number,
+    required: [true, 'Product price is required'],
+    validate: {
+      validator: function(value) {
+        return value > 0;
+      },
+      message: 'Product price must be greater than 0',
+    },
+  },
+  category: {
+    type: String,
+    required: [true, 'Product category is required'],
+  },
+  stock: {          
+    type: Number,
+    required:[true, 'Product stock is required'],
+    validate: {
+      validator: function(value) {
+        return value >= 0;
+      },
+      message: 'Product stock cannot be negative',
+    },
+  },
+  image: {
+    type: String,
+    required: [true, 'Product image is required'],
+  },
+  isDeleted: {
+    type: Boolean,
+    default: [false, 'Product is deleted'],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  deletedAt: {
+    type: Date,
+  }});
+
+  const Product = mongoose.model('Product', productSchema);
+  module.exports = Product;
