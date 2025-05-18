ccd39b9bf244a7d7877735839e0b1e3a801eeffc
xolani-pilot <xr.mkhwanazi.20@gmail.com>
2025-05-18 11:58:11 +0200

    feat: enable cors

--- api.js
+++ api.js
@@ -1,21 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors'); // ✅ Import cors
 const productRoute = require('./routes/product.route.js');
 
 const app = express();
+
+// ✅ Allow CORS from all origins
+app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products', productRoute);
+
 app.get('/', (req, res) => res.send('Hello World! from Express API'));
 
-// Mongo connection (only called once)
+// Mongo connection
 let isConnected = false;
 async function connectToDatabase() {
   if (isConnected) return;
   await mongoose.connect(
     'mongodb+srv://backendcluster0:XnB3Pu6jnG6ee12b@cluster0.fkkvbai.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0',
-
   );
   isConnected = true;
   console.log('MongoDB connected');
